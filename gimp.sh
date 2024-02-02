e7330472a3493d13c63cfcd0641d5fb3bb345901
Dotació Institut Numància <149525561+iesnumancia@users.noreply.github.com>
2024-02-02 18:37:33 +0100

    Update gimp.sh

--- gimp.sh
+++ gimp.sh
@@ -1,30 +1,14 @@
 #!/bin/bash
-
-# Comprobar si GIMP está instalado
+# Comprova si el GIMP està instal·lat
 if ! command -v gimp &> /dev/null; then
-    echo "GIMP no está instalado. Actualizando lista de dependencias y instalando GIMP..."
-    
-    # Actualizar la lista de dependencias e instalar GIMP
+    echo "El GIMP no està instal·lat. Actualitzant la llista de dependències i instal·lant el GIMP..."
+    # Actualitza la llista de dependències i instal·la el GIMP
     sudo apt update
     sudo apt install -y gimp
 else
-    echo "GIMP ya está instalado."
+    echo "El GIMP ja està instal·lat."
 fi
 
-# Verificar el idioma de GIMP y cambiar a español si es necesario
-if [ -z "$LANG" ] || [[ ! "$LANG" == *'es'* ]]; then
-    echo "Configurando GIMP en español..."
-    
-    # Establecer la variable de entorno del idioma de GIMP a español
-    export LANG=es_ES.UTF-8
-
-    # Puedes agregar aquí cualquier otro comando necesario para cambiar el idioma
-
-    echo "GIMP configurado en español."
-else
-    echo "GIMP ya está configurado en español."
-fi
-
-# Ejecutar GIMP
-echo "Ejecutando GIMP..."
+# Executa el GIMP
+echo "Executant el GIMP..."
 gimp
